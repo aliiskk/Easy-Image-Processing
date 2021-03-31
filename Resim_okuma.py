2d40981444d06ff72a748e6b88fca618b0951d26
aliiskk <78159970+aliiskk@users.noreply.github.com>
2021-03-31 11:48:02 +0300

    Delete Resim_okuma.py

--- Resim_okuma.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-
-img = cv2.imread("manzara.jpg",0) # 0=resmi siyah-beyaz yapar // # 1=default resim// # -1=alfa kanalı dahil olmak üzere yükler
-cv2.imshow("ResimAdi",img)  #imshow komutu = imageshow = resmi gösterdir; ilk önce pencereye verilecek ad sonra da gösterilecek resim yazılır
-cv2.waitKey(0)              # resmin kapanmamasını sağlar(ms)
-cv2.destroyAllWindows()     # windowsta herhangi bir tuşa basıldıgında kapanması sağlar
